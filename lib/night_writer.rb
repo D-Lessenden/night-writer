10d0ce4f411e0ae4144511c176a3e72a7fe1cd71
Daniel Lessenden <dless27@gmail.com>
2020-07-30 19:36:23 -0500

    Add files to the file tree.

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,19 @@
+class NightWriter
+attr_reader :file_reader
 
+  def initialize
+    @reader = FileReader.new
+  end
+
+  def encode_file_to_braille
+    plain = reader.read
+    braille = encode_to_braille(plain)
+  end
+
+  def encode_to_braille(input)
+
+  end
+
+end
+
+puts ARGV.inspect
