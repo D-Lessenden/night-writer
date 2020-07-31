cb90f108a605638b75c1298dd1723565fd4b141f
Daniel Lessenden <dless27@gmail.com>
2020-07-30 22:05:43 -0500

    Create new file when night_writer is ran

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -13,8 +13,13 @@ attr_reader :file_reader
   def encode_to_braille(input)
 
   end
+
+
+  # when command ran, takes the message file, translates it, and puts it into braille file.
 end
 
 
-#puts ARGV.inspect
+puts ARGV.inspect
 puts "Created '#{ARGV.last}' containing #{File.read(ARGV[0]).length} characters"
+#File.write(File.read(ARGV[0]), ARGV[1])  this makes a new folder with the name being the txt inside of arg0 and arg 1 being the text inside
+File.write(ARGV[1], File.read(ARGV[0]))
