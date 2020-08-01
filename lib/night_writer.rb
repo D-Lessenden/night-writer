35e715b935e212e38d9528136c5bf12fb56cc1dc
Daniel Lessenden <dless27@gmail.com>
2020-07-31 21:15:57 -0500

    Create runner file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -23,12 +23,5 @@ attr_reader :file_reader
   #
   # end
 
-
-
   # when command ran, takes the message file, translates it, and puts it into braille file.
 end
-
-
-# #puts ARGV.inspect
-  # puts "Created '#{ARGV.last}' containing #{File.read(ARGV[0]).length} characters"
-  # File.write(ARGV[1], File.read(ARGV[0])) #woop woop!
---
+++ runner.rb
@@ -0,0 +1,16 @@
+require './lib/night_writer'
+require './lib/file_reader'
+require './lib/encode'
+
+
+
+@nightwriter = NightWriter.new
+puts ARGV.inspect
+puts "Created '#{ARGV.last}' containing #{File.read(ARGV[0]).length} characters"
+File.write(ARGV[1], File.read(ARGV[0])) #woop woop!
+@nightwriter.encode_file_to_braille
+#the above creates a knew file and the text matches exactly that from message
+#need to pass in an argument so that braille is actually translated
+#probably the methods that beto and nico were using to write over the file
+#so first it will be english, a diff method is called and written over
+#to braille 
