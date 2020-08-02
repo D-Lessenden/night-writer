301fd3354d11d08282cb0636f6169f087333c9d7
Daniel Lessenden <dless27@gmail.com>
2020-08-02 03:01:26 -0500

    Add chomps to readers

--- runner_writer.rb
+++ runner_writer.rb
@@ -6,6 +6,6 @@ require './lib/encode'
 plain = ARGV[0]
 braille = ARGV[1]
 puts ARGV.inspect
-puts "Created '#{braille}' containing #{File.read(plain).length} characters"
+puts "Created '#{braille}' containing #{File.read(plain).chomp.length} characters"
 File.write(braille, File.read(plain)) #woop woop!
 @nightwriter.encode_file_to_braille
