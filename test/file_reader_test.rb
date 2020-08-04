299cd8738a73549421061fc3dbef07b4ea64e8cf
Daniel Lessenden <dless27@gmail.com>
2020-08-03 19:05:30 -0500

    Last of the refactoring... I hope

--- test/file_reader_test.rb
+++ test/file_reader_test.rb
@@ -2,12 +2,15 @@ require "./test/test_helper"
 require 'pry'
 require './lib/night_writer'
 require './lib/file_reader'
+require './lib/encode'
 
 
 class FileReaderTest < Minitest::Test
 
   def setup
     @reader = FileReader.new
+    @encode = Encode.new
+    @nightwriter = NightWriter.new
   end
 
   def test_it_exists
@@ -18,5 +21,15 @@ class FileReaderTest < Minitest::Test
     ARGV[0] = "message.txt"
     assert_equal "hello world\n", @reader.read
   end
+
+  def test_it_writes
+    ARGV[0] = "write_test.txt"
+    ARGV[1] = "write_test_braille.txt"
+    plain = @reader.read.downcase.chomp
+    braille = @nightwriter.encode(plain)
+    @reader.write(braille)
+    assert_equal ".00..0.0\n00.00.00\n0...0.0.", File.read("write_test_braille.txt")
+  end
+
 
 end
